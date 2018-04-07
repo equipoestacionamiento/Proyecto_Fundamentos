59c9eb22fc94598572b90d18402426bb26ffd7a9
equipoestacionamiento <fundamentospogramacion@gmail.com>
2018-04-07 00:03:11 -0500

    libreria stdlib

--- Banco_Occidente.cpp
+++ Banco_Occidente.cpp
@@ -2,6 +2,7 @@
 #include<iostream>
 #include<conio.h>
 #include<string.h>
+#include<stdlib.h>
 #define ENTER 13
 #define ESC 27
 using namespace std ;
@@ -13,29 +14,29 @@ string materia[7] = {"          Nombre:", "       Domicilio:", "Fecha Nacimiento
 
  main ()
 {
-	
-	
+
+
 ventana1:
-system ("cls");	
+system ("cls");
 cout<<"\t\t _____________________________________________________________"<<endl;
 cout<<"\t\t|                                                             |"<<endl;
-cout<<"\t\t|                 RAMON IGNACIO ARIAS MORFIN                  |"<<endl;	
-cout<<"\t\t|                JOSE JAVIER VILLALVAZO PEREZ                 |"<<endl;	
-cout<<"\t\t|                 MARCO ANTONIO PINON SAUCEDO                 |"<<endl;	
-cout<<"\t\t|                                                             |"<<endl;	
-cout<<"\t\t|                                                             |"<<endl;	
-cout<<"\t\t|                   UNITEC CAMPUS GUADALAJARA                 |"<<endl;	
-cout<<"\t\t|                  FUNDAMENTOS DE PROGRAMACION                |"<<endl;	
+cout<<"\t\t|                 RAMON IGNACIO ARIAS MORFIN                  |"<<endl;
+cout<<"\t\t|                JOSE JAVIER VILLALVAZO PEREZ                 |"<<endl;
+cout<<"\t\t|                 MARCO ANTONIO PINON SAUCEDO                 |"<<endl;
+cout<<"\t\t|                                                             |"<<endl;
+cout<<"\t\t|                                                             |"<<endl;
+cout<<"\t\t|                   UNITEC CAMPUS GUADALAJARA                 |"<<endl;
+cout<<"\t\t|                  FUNDAMENTOS DE PROGRAMACION                |"<<endl;
 cout<<"\t\t|                     PROFESOR: CESAR PARRA                   |"<<endl;
-cout<<"\t\t|                                                             |"<<endl;	
-cout<<"\t\t|                                                             |"<<endl;	
+cout<<"\t\t|                                                             |"<<endl;
+cout<<"\t\t|                                                             |"<<endl;
 cout<<"\t\t|       ____________________             _______              |"<<endl;
 cout<<"\t\t|      | PRESIONE UNA TECLA |           |  ESC  |             |"<<endl;
 cout<<"\t\t|      |   PARA CONTINUAR   |           |(SALIR)|             |"<<endl;
 cout<<"\t\t|      |____________________|           |_______|             |"<<endl;
 cout<<"\t\t|                                                             |"<<endl;
 cout<<"\t\t|                                                             |"<<endl;
-cout<<"\t\t|_____________________________________________________________|"<<endl;	
+cout<<"\t\t|_____________________________________________________________|"<<endl;
 
 
 
@@ -43,64 +44,64 @@ tecla = getch();
 if(tecla==ENTER)
 {
 	goto ventana2;
-	
-	}	
+
+	}
 if(tecla==ESC)
 {
 	cout<<"ADIOS";
 	return 0;
-	
+
 	}
-	
+
 ventana2:
-system ("cls");	
+system ("cls");
 cout<<"\t\t _____________________________________________________________"<<endl;
 cout<<"\t\t|                                                             |"<<endl;
-cout<<"\t\t|                                                             |"<<endl;	
-cout<<"\t\t|                BANCO DE OCCIDENTE S.A DE C.V                |"<<endl;	
-cout<<"\t\t|                                                             |"<<endl;	
-cout<<"\t\t|                                                             |"<<endl;	
-cout<<"\t\t|                                                             |"<<endl;	
-cout<<"\t\t|                   DESEA INGRESAR AL SISTEMA                 |"<<endl;	
-cout<<"\t\t|                   (PRESIONE CUALQUIER TECLA)                |"<<endl;	
 cout<<"\t\t|                                                             |"<<endl;
-cout<<"\t\t|                                                             |"<<endl;	
-cout<<"\t\t|                                                             |"<<endl;	
+cout<<"\t\t|                BANCO DE OCCIDENTE S.A DE C.V                |"<<endl;
+cout<<"\t\t|                                                             |"<<endl;
+cout<<"\t\t|                                                             |"<<endl;
+cout<<"\t\t|                                                             |"<<endl;
+cout<<"\t\t|                   DESEA INGRESAR AL SISTEMA                 |"<<endl;
+cout<<"\t\t|                   (PRESIONE CUALQUIER TECLA)                |"<<endl;
+cout<<"\t\t|                                                             |"<<endl;
+cout<<"\t\t|                                                             |"<<endl;
+cout<<"\t\t|                                                             |"<<endl;
 cout<<"\t\t|                          REGRESAR                           |"<<endl;
 cout<<"\t\t|                       (Presione ESC)                        |"<<endl;
 cout<<"\t\t|                                                             |"<<endl;
 cout<<"\t\t|                                                             |"<<endl;
 cout<<"\t\t|                                                             |"<<endl;
 cout<<"\t\t|                                                             |"<<endl;
-cout<<"\t\t|_____________________________________________________________|"<<endl;	
+cout<<"\t\t|_____________________________________________________________|"<<endl;
 
-	
+
 tecla2=getch();
 if(tecla2==ENTER)
 {
 	goto ventana3;
-	
-	}	
+
+	}
 if(tecla2==ESC)
 {
 	goto ventana1;
-	
+
 	}
 
 ventana3:
 system ("cls");
 cout<<"\t\t _____________________________________________________________"<<endl;
 cout<<"\t\t|                                                             |"<<endl;
-cout<<"\t\t|                             MENU                            |"<<endl;	
-cout<<"\t\t|                                                             |"<<endl;	
-cout<<"\t\t|                 1) INGRESAR NUEVO CLIENTE                   |"<<endl;	
-cout<<"\t\t|                                                             |"<<endl;	
-cout<<"\t\t|                 2) ELIMINAR REGISTRO DE CLIENTE             |"<<endl;	
-cout<<"\t\t|                                                             |"<<endl;	
-cout<<"\t\t|                 3) MODIFICAR REGISTRO DE CLIENTE            |"<<endl;	
+cout<<"\t\t|                             MENU                            |"<<endl;
 cout<<"\t\t|                                                             |"<<endl;
-cout<<"\t\t|                 4) MOSTRAR REGISTRO  DE CLIENTE             |"<<endl;			
-cout<<"\t\t|_____________________________________________________________|"<<endl;	
+cout<<"\t\t|                 1) INGRESAR NUEVO CLIENTE                   |"<<endl;
+cout<<"\t\t|                                                             |"<<endl;
+cout<<"\t\t|                 2) ELIMINAR REGISTRO DE CLIENTE             |"<<endl;
+cout<<"\t\t|                                                             |"<<endl;
+cout<<"\t\t|                 3) MODIFICAR REGISTRO DE CLIENTE            |"<<endl;
+cout<<"\t\t|                                                             |"<<endl;
+cout<<"\t\t|                 4) MOSTRAR REGISTRO  DE CLIENTE             |"<<endl;
+cout<<"\t\t|_____________________________________________________________|"<<endl;
 cin>>menu;
 
 switch (menu)
@@ -108,18 +109,18 @@ switch (menu)
 	case 1:
 		 ingresar();
 	break;
-	
+
 	case 2:
 	//	goto eliminar;
-	break;	
-	
+	break;
+
 	case 3:
 	//	goto modificar;
 	break;
-	
+
 	case 4:
 	//	goto mostrar;
-	break;	
+	break;
 }
 
 /*
@@ -130,8 +131,8 @@ modificar:
 	cout<<"hola3";
 return 0;
 mostrar:
-    cout<<"hola4";	
-return 0;	
+    cout<<"hola4";
+return 0;
 */
 
 
@@ -141,11 +142,11 @@ void ingresar()
 {
 	system("cls");
 	cout<<"\t\tIngrese sus datos"<<endl<<endl;
-for (int j=0; j<7; j++){	
+for (int j=0; j<7; j++){
 		cout<<materia[j]<<"\t";
-		cin>>cliente1[j]; 
+		cin>>cliente1[j];
 		cout<<"\n";	}
-		
+
 	getch();
 
 }
