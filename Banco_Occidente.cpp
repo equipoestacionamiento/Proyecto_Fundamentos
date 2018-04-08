7e59823aa130e34f9bea5c43b20ae176446d72c7
equipoestacionamiento <fundamentospogramacion@gmail.com>
2018-04-08 16:10:28 -0500

    struct ingresar cliente

--- Banco_Occidente.cpp
+++ Banco_Occidente.cpp
@@ -1,59 +1,66 @@
-#include<stdio.h>
 #include<iostream>
-#include<conio.h>
 #include<string.h>
-#include<stdlib.h>
+#include<conio.h>
+#include<stdio.h>
+
 #define ENTER 13
 #define ESC 27
-using namespace std ;
-char tecla,tecla2;
-int menu;
-void ingresar();
-string cliente1[2];
-string materia[7] = {"          Nombre:", "       Domicilio:", "Fecha Nacimiento:", "            Edad:","          Correo:", "  Tipo de cuenta:", "Saldo disponible:"};
 
- main ()
-{
-system("color 3A");
+struct cliente{
+	char nombre[100];
+	char fechanacimiento[100];
+	int edad;
+	char correo[100];
+	char tipocuenta[100];
+	double saldo;
+	char domicilio[100];
+	char ciudad[50];
+}clientes[5];
 
+//string materia[7] = {"          Nombre:", "       Domicilio:", "Fecha Nacimiento:", "            Edad:","          Correo:", "  Tipo de cuenta:", "Saldo disponible:"};
+
+using namespace std;
+
+int main(){
+
+	int tecla,tecla2;
+	int menu,n,i;
 ventana1:
-system ("cls");
-cout<<"\t\t _____________________________________________________________"<<endl;
-cout<<"\t\t|                                                             |"<<endl;
-cout<<"\t\t|                 RAMON IGNACIO ARIAS MORFIN                  |"<<endl;
-cout<<"\t\t|                JOSE JAVIER VILLALVAZO PEREZ                 |"<<endl;
-cout<<"\t\t|                 MARCO ANTONIO PINON SAUCEDO                 |"<<endl;
-cout<<"\t\t|                                                             |"<<endl;
-cout<<"\t\t|                                                             |"<<endl;
-cout<<"\t\t|                   UNITEC CAMPUS GUADALAJARA                 |"<<endl;
-cout<<"\t\t|                  FUNDAMENTOS DE PROGRAMACION                |"<<endl;
-cout<<"\t\t|                     PROFESOR: CESAR PARRA                   |"<<endl;
-cout<<"\t\t|                                                             |"<<endl;
-cout<<"\t\t|                                                             |"<<endl;
-cout<<"\t\t|       ____________________             _______              |"<<endl;
-cout<<"\t\t|      | PRESIONE UNA TECLA |           |  ESC  |             |"<<endl;
-cout<<"\t\t|      |   PARA CONTINUAR   |           |(SALIR)|             |"<<endl;
-cout<<"\t\t|      |____________________|           |_______|             |"<<endl;
-cout<<"\t\t|                                                             |"<<endl;
-cout<<"\t\t|                                                             |"<<endl;
-cout<<"\t\t|_____________________________________________________________|"<<endl;
+	system ("cls");
+	cout<<"\t\t _____________________________________________________________"<<endl;
+	cout<<"\t\t|                                                             |"<<endl;
+	cout<<"\t\t|                 RAMON IGNACIO ARIAS MORFIN                  |"<<endl;
+	cout<<"\t\t|                JOSE JAVIER VILLALVAZO PEREZ                 |"<<endl;
+	cout<<"\t\t|                 MARCO ANTONIO PINON SAUCEDO                 |"<<endl;
+	cout<<"\t\t|                                                             |"<<endl;
+	cout<<"\t\t|                                                             |"<<endl;
+	cout<<"\t\t|                   UNITEC CAMPUS GUADALAJARA                 |"<<endl;
+	cout<<"\t\t|                  FUNDAMENTOS DE PROGRAMACION                |"<<endl;
+	cout<<"\t\t|                     PROFESOR: CESAR PARRA                   |"<<endl;
+	cout<<"\t\t|                                                             |"<<endl;
+	cout<<"\t\t|                                                             |"<<endl;
+	cout<<"\t\t|       ____________________             _______              |"<<endl;
+	cout<<"\t\t|      | PRESIONE UNA TECLA |           |  ESC  |             |"<<endl;
+	cout<<"\t\t|      |   PARA CONTINUAR   |           |(SALIR)|             |"<<endl;
+	cout<<"\t\t|      |____________________|           |_______|             |"<<endl;
+	cout<<"\t\t|                                                             |"<<endl;
+	cout<<"\t\t|                                                             |"<<endl;
+	cout<<"\t\t|_____________________________________________________________|"<<endl;
 
 
-
+//ventana2
 tecla = getch();
 if(tecla==ENTER)
 {
 	goto ventana2;
-
-	}
-if(tecla==ESC)
+	
+	}	
+else if(tecla==ESC)
 {
-	cout<<"ADIOS";
 	return 0;
-
 	}
-
-ventana2:
+	
+	ventana2:
 system ("cls");
 cout<<"\t\t _____________________________________________________________"<<endl;
 cout<<"\t\t|                                                             |"<<endl;
@@ -75,20 +82,17 @@ cout<<"\t\t|                                                             |"<<end
 cout<<"\t\t|                                                             |"<<endl;
 cout<<"\t\t|_____________________________________________________________|"<<endl;
 
-
-tecla2=getch();
-if(tecla2==ENTER)
+tecla = getch();
+if(tecla==ENTER)
 {
 	goto ventana3;
-
-	}
-if(tecla2==ESC)
+	
+	}	
+else if(tecla==ESC)
 {
 	goto ventana1;
-
 	}
-
-ventana3:
+	ventana3:
 system ("cls");
 cout<<"\t\t _____________________________________________________________"<<endl;
 cout<<"\t\t|                                                             |"<<endl;
@@ -104,49 +108,41 @@ cout<<"\t\t|                 4) MOSTRAR REGISTRO  DE CLIENTE             |"<<end
 cout<<"\t\t|_____________________________________________________________|"<<endl;
 cin>>menu;
 
-switch (menu)
-{
-	case 1:
-		 ingresar();
+switch(menu)
+	{
+	 case 1:
+	 	system("cls");
+	  
+	  for( i=0; i<2; i++){
+	  	fflush(stdin);
+	cout<<"cliente # "<<i+1<<endl;
+	cout<<"\n           Nombre: \t"; 
+	cin.getline(clientes[i].nombre , 100, '\n');
+	cout<<"\n        Domicilio: \t ";
+	cin.getline(clientes[i].domicilio, 100, '\n');
+	cout<<"\n	Fecha Nacimiento:(DD/MM/AAAA) \t ";
+	cin.getline(clientes[i].fechanacimiento, 100, '\n');
+	    
+					}
+					
+	
 	break;
-
-	case 2:
-	//	goto eliminar;
+	 
+	 case 2:
 	break;
-
-	case 3:
-	//	goto modificar;
+	
+	 case 3:
 	break;
-
-	case 4:
-	//	goto mostrar;
+	
+	 case 4:
 	break;
-}
-
-/*
-eliminar:
-	cout<<"hola2";
-return 0;
-modificar:
-	cout<<"hola3";
-return 0;
-mostrar:
-    cout<<"hola4";
-return 0;
-*/
+	}	
 
 
-}
+	
 
-void ingresar()
-{
-	system("cls");
-	cout<<"\t\tIngrese sus datos"<<endl<<endl;
-for (int j=0; j<7; j++){
-		cout<<materia[j]<<"\t";
-		cin>>cliente1[j];
-		cout<<"\n";	}
-
-	getch();
-
-}
+		
+	return 0;
+	
+	}
+	
